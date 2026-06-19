1f257f2304377732fceecdb34e3f1bd4b46e2201
woshiniaoshen <tannelson2811@gmail.com>
2026-06-19 21:09:17 +0800

    fix cloudflare deployment config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
 
-const rootDir = fileURLToPath(new URL('.', import.meta.url))
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   build: {
     rollupOptions: {
       input: {
-        portfolio: resolve(rootDir, 'index.html'),
-        footprint: resolve(rootDir, 'footprint/index.html'),
+        portfolio: resolve('index.html'),
+        footprint: resolve('footprint/index.html'),
       },
     },
   },
